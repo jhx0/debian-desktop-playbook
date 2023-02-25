6686c46d1ed6d1ccae9a43cf33f0e29f93c6a3ed
jhx <jhx0x00@gmail.com>
2023-02-25 19:26:55 +0100

    major rework of the entire playbook (rework/tags added)

--- roles/firewall/files/fw.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-
-iptables -P OUTPUT ACCEPT
-iptables -P INPUT DROP
-iptables -A INPUT -m conntrack --ctstate RELATED,ESTABLISHED -j ACCEPT
-iptables -A INPUT -i lo -j ACCEPT
-iptables -A INPUT -p tcp --dport 22 -j ACCEPT
